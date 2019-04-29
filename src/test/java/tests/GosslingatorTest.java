72e75dd7978a1bb5d18d01f268ef77cd33f0acf3
JankaKos <tenisek@gmail.com>
2019-04-29 19:49:04 +0200

    vynatie tlacidla do samostatnej premennej

--- src/test/java/tests/GosslingatorTest.java
+++ src/test/java/tests/GosslingatorTest.java
@@ -1,47 +1,117 @@
 package tests;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GosslingatorTest<http> {
+    WebDriver driver;
 
- @Test
- public void itShouldAddOneRyan(){
-  System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-  //0.spustit prehliadac
-  WebDriver driver = new ChromeDriver();
-     //1.otvorit stranku
-  driver.get("http://localhost/gosslingator.php");
-     //2.kliknut na button Ryan
-  driver.findElement(By.id("addRyan")).click();
-     //3.overit pocitanie Ryanov
-  Assert.assertEquals("1", driver.findElement(By.id("ryanCounter")).getText());
-     //4.zavriet prehliadac
-  driver.close();
-     //5.ukoncit session
-  driver.quit();
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver74.exe");
+        //0.spustit prehliadac
+        driver = new ChromeDriver();
+        //1.otvorit stranku
+        driver.get("http://localhost/gosslingator.php");
+    }
 
- }
+    @Test
+    public void itShouldRyan() {
+        //2.kliknut na button Ryan
+        driver.findElement(By.id("addRyan")).
 
- @Test
- public void itShouldAddTwoRyan(){
-  System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-  //0.spustit prehliadac
-  WebDriver driver = new ChromeDriver();
-  //1.otvorit stranku
-  driver.get("http://localhost/gosslingator.php");
-  //2.kliknut 2x na button Ryan
-  driver.findElement(By.id("addRyan")).click();
-  driver.findElement(By.id("addRyan")).click();
-  //3.overit pocitanie Ryanov
-  Assert.assertEquals("2", driver.findElement(By.id("ryanCounter")).getText());
-  //4.zavriet prehliadac
-  driver.close();
-  //5.ukoncit session
-  driver.quit();
+                click();
+        //3.overit pocitanie Ryanov
+        Assert.assertEquals("1", driver.findElement(By.id("ryanCounter")).
 
- }
+                getText());
+        //4.zavriet prehliadac
+        driver.close();
+        //5.ukoncit session
+        driver.quit();
+
+    }
+
+    @Test
+    public void itShouldAddTwoRyan() {
+        WebElement addRyanButton = driver.findElement(By.id("addRyan"));
+        //2.kliknut 2x na button Ryan
+        addRyanButton.click();
+        addRyanButton.click();
+        //3.overit pocitanie Ryanov
+        Assert.assertEquals("2", driver.findElement(By.id("ryanCounter")).getText());
+        //4.zavriet prehliadac
+        driver.close();
+        //5.ukoncit session
+        driver.quit();
+
+    }
+
+
+
+
+    @Test
+    public void itShouldItDisplayWarningMessage() {
+        WebElement addRyanButton = driver.findElement(By.id("addRyan"));
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+        addRyanButton.click();
+
+
+    }
+
 }
+
