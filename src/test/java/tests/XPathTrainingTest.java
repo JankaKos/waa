d6e19ad2ed4f9ddf5c8106d4f61f8ecc9d4b0b3e
JankaKos <tenisek@gmail.com>
2019-05-13 19:48:11 +0200

    cykly

--- src/test/java/tests/XPathTrainingTest.java
+++ src/test/java/tests/XPathTrainingTest.java
@@ -1,15 +1,18 @@
 package tests;
 
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class XPathTrainingTest {
-    WebDriver driver;
+    WebDriver driver = null;
 
     @Before
     public void setUp() {
@@ -17,11 +20,10 @@ public class XPathTrainingTest {
         //0.spustit prehliadac
         driver = new ChromeDriver();
         //1.otvorit stranku
-        driver.get("http://localhost/xpathtrainingcenter.php");
+        driver.get("http://localhost:81/xpathtrainingcenter.php");
 
 
     }
-
     @After
     public void tearDown() {
         driver.close();
@@ -34,10 +36,8 @@ public class XPathTrainingTest {
         String buttonText = "One more button";
         driver.findElement(By.xpath("//button[contains(text(), '" + buttonText + "')]")).click();
         String actualMessage = driver.findElement(By.cssSelector("div.output h2 span")).getText();
-
         Assert.assertEquals(
                 "you clicked " + buttonText.toLowerCase(), actualMessage
-
         );
     }
 
@@ -55,4 +55,45 @@ public class XPathTrainingTest {
                 "you entered " + message,
                 actualMessage);
     }
+
+    @Test
+    public void itShouldClickAllButtons() {
+
+        String[] selectedButtons = {"First one", "Second button", "Next button", "One more button", "Danger", "Success", "Warning"};
+
+        for (String button : selectedButtons) {
+            WebElement buttonSelect = driver.findElement(By.xpath("//button[contains(text(), '" + button + "')]"));
+            buttonSelect.click();
+            String expectedMessage = "you clicked " + button.toLowerCase();
+            String actualMessage = driver.findElement(By.cssSelector("div.output h2 span")).getText();
+            Assert.assertEquals(expectedMessage, actualMessage);
+        }
+    }
+
+    @Test
+    public void itShouldDisplayEverySelect() {
+        String[] moznostiSelected = {"vyber si moznost", "Moznost 1", "Moznost 2", "Moznost 3", "Moznost 4", "Moznost 5", "Moznost 6"};
+        WebElement selectedMoznost = driver.findElement(By.xpath("//select[1]"));
+        for (int i = 6; i >= 0 ; i--) {
+            new Select(selectedMoznost).selectByIndex(i);
+            String s = moznostiSelected[i];
+            String expectedMessage = "you have chosen " + s.toLowerCase();
+            String actualMessage = driver.findElement(By.cssSelector("div.output h2 span")).getText();
+            Assert.assertEquals(expectedMessage, actualMessage);
+        }
+    }
+
+    @Test
+    public void itShouldDisplayEverySelectNumberTwo() {
+        String[] moznostiSelected = {"vyber si option", "Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Option 6"};
+        WebElement selectedMoznost = driver.findElement(By.xpath("//select[2]"));
+        for (int i = 6; i >= 0; i--) {
+            new Select(selectedMoznost).selectByIndex(i);
+            String s = moznostiSelected[i];
+            String expectedMessage = "you have chosen " + s.toLowerCase();
+            String actualMessage = driver.findElement(By.cssSelector("div.output h2 span")).getText();
+            Assert.assertEquals(expectedMessage, actualMessage);
+        }
+    }
+
 }
