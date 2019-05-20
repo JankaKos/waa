0eedf44f9eaf3b021687cd48c4ec80d2fee50b7f
JankaKos <tenisek@gmail.com>
2019-05-20 19:37:56 +0200

    refaktorizacia pomocou metod

--- src/test/java/tests/OdkazovacTest.java
+++ src/test/java/tests/OdkazovacTest.java
@@ -22,24 +22,34 @@ public class OdkazovacTest extends TestBase {
         String titulok = "titulok " + timestamp.getTime();
         String mojeMeno = "Moje meno";
         String textOdkazu = "Ahoj, dakujem za prispevok";
+        enterNoteData(titulok,mojeMeno,textOdkazu);
+        submitNewNote();
+        Assert.assertTrue(driver.findElement(By.cssSelector("li.sin ")).isDisplayed());
+        Assert.assertTrue(getLastNoteFromList().isDisplayed());
+        getLastNoteFromList().click();
+        Thread.sleep(3000);
+        //bonus
+        checkNoteDetail(titulok, mojeMeno, textOdkazu);
+    }
 
+    private void enterNoteData(String titulok, String mojeMeno, String textOdkazu){
         driver.findElement(By.name("title")).sendKeys(titulok);
         driver.findElement(By.name("author")).sendKeys(mojeMeno);
         driver.findElement(By.name("message")).sendKeys(textOdkazu);
-
+    }
+    private void submitNewNote(){
         driver.findElement(By.cssSelector("button.btn-block")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("li.sin ")).isDisplayed());
-        WebElement detailButton = driver.findElement(By.cssSelector("ul.list-of-sins li:last-child"));
-        Assert.assertTrue(detailButton.isDisplayed());
-        detailButton.click();
-        Thread.sleep(3000);
+    }
 
-        //bonus
-        WebElement detailOdkazu = driver.findElement(By.cssSelector("div.content"));
-        Assert.assertEquals(titulok, detailOdkazu.findElement(By.cssSelector("h4.title")).getText());
-        Assert.assertEquals(mojeMeno, detailOdkazu.findElement(By.cssSelector("h4.recipent")).getText());
-        Assert.assertEquals(textOdkazu, detailOdkazu.findElement(By.cssSelector("p")).getText());
+    private WebElement getLastNoteFromList(){
+       return driver.findElement(By.cssSelector("ul.list-of-sins li:last-child"));
+    }
 
+    private  void checkNoteDetail(String  title, String author, String message ) {
+        WebElement detail = driver.findElement(By.cssSelector("div.content"));
+        Assert.assertEquals(title, detail.findElement(By.cssSelector("h4.title")).getText());
+        Assert.assertEquals(author, detail.findElement(By.cssSelector("h4.recipent")).getText());
+        Assert.assertEquals(message, detail.findElement(By.cssSelector("p")).getText());
     }
 
 }
