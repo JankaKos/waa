241964603263f41f278991d4a7bdea820eece656
JankaKos <tenisek@gmail.com>
2019-05-20 18:19:13 +0200

    factorizacia kodu pomocou metod

--- src/test/java/tests/GosslingatorTest.java
+++ src/test/java/tests/GosslingatorTest.java
@@ -23,60 +23,46 @@ public class GosslingatorTest extends TestBase {
     private String actualNumberOfRyans;
 
     @Test
-    public void itShouldRyan() {
+    public void itShouldAddRyan() {
         //2.kliknut na button Ryan
-        driver.findElement(By.id("addRyan")).
-
-                click();
+        addRyan();
         //3.overit pocitanie Ryanov
-        actualNumberOfRyans = driver.findElement(By.id("ryanCounter")).getText();
-        Assert.assertEquals("1", actualNumberOfRyans);
-        System.out.println("Number of ryans: " + driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
+        Assert.assertEquals("1", actualNumberOfRyans());
+        System.out.println("Number of ryans: " + actualNumberOfRyans());
 
-        Assert.assertEquals("ryan", driver.findElement(By.cssSelector("div.ryan-counter h3")).getText());
+        Assert.assertEquals("ryan", actualDescription());
 
 
     }
 
     @Test
     public void itShouldAddTwoRyan() {
-        WebElement addRyanButton = driver.findElement(By.id("addRyan"));
-        //2.kliknut 2x na button Ryan
-        addRyanButton.click();
-        addRyanButton.click();
+        addRyan();
+        addRyan();
         //3.overit pocitanie Ryanov
-        actualNumberOfRyans = driver.findElement(By.id("ryanCounter")).getText();
-        String actualRyanDescription = driver.findElement(By.cssSelector("div.ryan-counter h3")).getText();
-        Assert.assertEquals("2", actualNumberOfRyans);
-        Assert.assertEquals("ryans", actualRyanDescription);
-
+        Assert.assertEquals("2", actualNumberOfRyans());
+        Assert.assertEquals("ryans", actualDescription());
     }
 
     @Test
 
     public void itShouldDisplayTitle() {
         System.out.println(driver.findElement(By.cssSelector(".ryan-title")).getText());
-
         Assert.assertEquals("GOSLINGATE ME", driver.findElement(By.cssSelector(".ryan-title")).getText());
     }
 
 
     @Test
     public void itShouldItDisplayWarningMessage() {
-
-        WebElement addRyanButton = driver.findElement(By.id("addRyan"));
         for (int i = 0; i < 50; i++) {
-            int numberOfRyanImages = driver.findElements(By.cssSelector("img")).size();
-            Assert.assertEquals(i,numberOfRyanImages);
-            addRyanButton.click();
-            actualNumberOfRyans = driver.findElement(By.id("ryanCounter")).getText();
-            Assert.assertEquals(String.valueOf(i + 1), actualNumberOfRyans);
-            String actualDescription = driver.findElement(By.cssSelector("div.ryan-counter h3")).getText();
+            Assert.assertEquals(i, getNumberOfRyanImages());
+            addRyan();
+            Assert.assertEquals(String.valueOf(i + 1), actualNumberOfRyans());
             if (i + 1 == 1) {
-                Assert.assertEquals("ryan", actualDescription);
+                Assert.assertEquals("ryan", actualDescription());
             }
             if (i + 1 >= 2) {
-                Assert.assertEquals("ryans", actualDescription);
+                Assert.assertEquals("ryans", actualDescription());
             }
         }
 
@@ -90,21 +76,17 @@ public class GosslingatorTest extends TestBase {
 
                         "HIGH",
 
-                driver.findElement(By.cssSelector("h1.tooManyRyans")).
-
-                        getText());
-
+                driver.findElement(By.cssSelector("h1.tooManyRyans")).getText());
     }
 
     @Test
     public void itShouldDisplayWarningMessageUsingWhileCycle() {
-        WebElement addRyanButton = driver.findElement(By.id("addRyan"));
-        String actualNumberOfRyans = driver.findElement(By.id("ryanCounter")).getText();
+
+        actualNumberOfRyans();
         int clicksLimit= 30;
         int clicks = 0;
-        while (!actualNumberOfRyans.equals("50")|| clicks <= clicksLimit ) {
-            addRyanButton.click();
-            actualNumberOfRyans = driver.findElement(By.id("ryanCounter")).getText();
+        while (!actualNumberOfRyans().equals("50")|| clicks <= clicksLimit ) {
+            addRyan();
             clicks++;
         }
     }
@@ -112,10 +94,21 @@ public class GosslingatorTest extends TestBase {
     @Test
     public void itShouldDisplayNoRyanOnPageOpe(){
 
-        Assert.assertEquals(0,driver.findElements(By.cssSelector("img")).size());
+        Assert.assertEquals(0,getNumberOfRyanImages());
 
+    }
 
-
+    private void addRyan (){
+        driver.findElement(By.id("addRyan")).click();
+    }
+    private String actualNumberOfRyans(){
+       return driver.findElement(By.id("ryanCounter")).getText();
+    }
+    private String actualDescription(){
+        return  driver.findElement(By.cssSelector("div.ryan-counter h3")).getText();
+    }
+    private int getNumberOfRyanImages(){
+        return driver.findElements(By.cssSelector("img")).size();
     }
 }
 
