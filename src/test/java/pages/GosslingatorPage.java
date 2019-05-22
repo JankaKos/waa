cf02628177ec9da99726f8c78cd616486a5fb18a
JankaKos <tenisek@gmail.com>
2019-05-22 19:44:48 +0200

    PgaFactory.initElements zadefinovanie si elementov

--- src/test/java/pages/GosslingatorPage.java
+++ src/test/java/pages/GosslingatorPage.java
@@ -3,23 +3,33 @@ package pages;
 import base.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class GosslingatorPage {
     WebDriver pageDriver;
 
+    @FindBy (id = "addRyan")
+    private WebElement addRyanButton;
+    @FindBy(css="div.ryan-counter h3")
+    private WebElement ryanCounter;
+
+
     public GosslingatorPage(WebDriver driver) {
         this.pageDriver = driver;
+        PageFactory.initElements(pageDriver,this);
     }
 
 
     public void addRyan() {
-        pageDriver.findElement(By.id("addRyan")).click();
+        addRyanButton.click();
     }
     public String actualNumberOfRyans(){
         return pageDriver.findElement(By.id("ryanCounter")).getText();
     }
     public String actualDescription(){
-        return  pageDriver.findElement(By.cssSelector("div.ryan-counter h3")).getText();
+        return ryanCounter.getText();
     }
     public int getNumberOfRyanImages(){
         return pageDriver.findElements(By.cssSelector("img")).size();
--- src/test/java/tests/GosslingatorTest.java
+++ src/test/java/tests/GosslingatorTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 import pages.GosslingatorPage;
 
 public class GosslingatorTest extends TestBase {
