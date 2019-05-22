8435016d3feb9290c0404c5e8d9bb7fcdef73e78
JankaKos <tenisek@gmail.com>
2019-05-22 17:39:15 +0200

    domaca uloha

--- src/test/java/tests/FellowshipTest.java
+++ src/test/java/tests/FellowshipTest.java
@@ -16,46 +16,46 @@ public class FellowshipTest extends TestBase {
 
     @Before
     public void openPage() {
-        driver.get( BASE_URL + "/fellowship.php");
+        driver.get(BASE_URL + "/fellowship.php");
     }
 
-
-@Test
-    public void itShouldContainNameForEachFellow(){
-    List<WebElement> fellowElements =driver.findElements(By.cssSelector("ul.list-of-fellows li"));
-    for (WebElement fellowElement : fellowElements) {
-        String nameFellow = fellowElement.findElement(By.cssSelector("h1")).getText();
-
-        Assert.assertFalse(nameFellow.isEmpty());
-    }
-    //System.out.println("ahoj");
-}
 
     @Test
-    public void itShouldContainSpecifiedFellow(){
-        List<WebElement> fellowElements =driver.findElements(By.cssSelector("ul.list-of-fellows li"));
+    public void itShouldContainNameForEachFellow() {
+        List<WebElement> fellowElements = getElements();
+        for (WebElement fellowElement : fellowElements) {
+            Assert.assertFalse(itShouldFindElement(fellowElement).isEmpty());
+        }
+        //System.out.println("ahoj");
+    }
+
+    @Test
+    public void itShouldContainSpecifiedFellow() {
+        List<WebElement> fellowElements = getElements();
         List<String> fellowNames = new ArrayList<String>();
 
         for (WebElement fellowElement : fellowElements) {
-            fellowNames.add(fellowElement.findElement(By.cssSelector("h1")).getText());
+            fellowNames.add(itShouldFindElement(fellowElement));
         }
         System.out.println(fellowNames);
         Assert.assertTrue(fellowNames.contains("Gandalf"));
         Assert.assertTrue(fellowNames.contains("Aragorn"));
         Assert.assertTrue(fellowNames.contains("Frodo"));
-        }
-        //System.out.println("ahoj");
+    }
+
+
+    //System.out.println("ahoj");
 
     @Test
-    public void itShouldDisplayComplete (){
+    public void itShouldDisplayComplete() {
         String[] fellowToSelected = {"Samwise", "Gimli", "Gandalf", "Aragorn"};
         for (String fellow : fellowToSelected) {
-            driver.findElement(By.xpath("//div[h1[contains(text(), '" + fellow + "')]]")).click();
+            itShouldClickFellow(fellow);
         }
-            Assert.assertEquals("complete", driver.findElement(By.cssSelector("div.points-left h3"))
-            .getText()
-            .toLowerCase());
-        }
+        Assert.assertEquals("complete", driver.findElement(By.cssSelector("div.points-left h3"))
+                .getText()
+                .toLowerCase());
+    }
 
     @Test
     public void itShouldDisplayCountOf() {
@@ -67,14 +67,28 @@ public class FellowshipTest extends TestBase {
 
     @Test
     public void itShouldDisplayPointsForEachFellow() {
-        List<WebElement> displayedFellows = driver.findElements(By.cssSelector("ul.list-of-fellows li"));
+        List<WebElement> displayedFellows = getElements();
 
         for (WebElement displayedFellow : displayedFellows) {
             String actualPoints = displayedFellow.findElement(By.cssSelector("div.fellow-points h2")).getText();
             Assert.assertFalse(actualPoints.isEmpty());
-
         }
 
+    }
+
+
+    private void itShouldClickFellow(String name) {
+        driver.findElement(By.xpath("//div[h1[contains(text(), '" + name + "')]]")).click();
+
+    }
+
+    private String itShouldFindElement(WebElement element) {
+        return element.findElement(By.cssSelector("h1")).getText();
+    }
+
+
+    private List<WebElement> getElements() {
+        return driver.findElements(By.cssSelector("ul.list-of-fellows li"));
     }
 
 }
