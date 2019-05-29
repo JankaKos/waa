af98e1dd8050680200468d3f6cd2cb6094d5f93f
JankaKos <tenisek@gmail.com>
2019-05-29 14:06:28 +0200

    otestovanie zakladnych funkcii kalkulacky, vytvorenie metod, konstruktora CalculatorPage

---
+++ src/test/java/pages/CalculatorPage.java
@@ -0,0 +1,47 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class CalculatorPage {
+    WebDriver pageDriver;
+
+
+    public CalculatorPage (WebDriver driver){
+        this.pageDriver = driver;
+        PageFactory.initElements(pageDriver,this);
+    }
+
+    @FindBy(id = "firstInput")
+    WebElement firstInput;
+    @FindBy(id = "secondInput")
+    WebElement secondInput;
+
+    public void sendNumbers(String sendFirstNumber, String sendSecondNumber){
+        firstInput.sendKeys(sendFirstNumber);
+        secondInput.sendKeys(sendSecondNumber);
+    }
+
+    public WebElement getFirstInput(){
+        return firstInput;
+    }
+
+    public WebElement getSecondInput(){
+        return secondInput;
+    }
+    public String actualResult (){
+        return pageDriver.findElement(By.id("result")).getText();
+    }
+
+    public WebElement getLatestCalculation() {
+        return pageDriver.findElement(By.cssSelector("ul.latest-results li"));
+    }
+    public List<WebElement> getListOfCalculation(){
+        return pageDriver.findElements(By.cssSelector("ul.latest-results li"));
+    }
+}
---
+++ src/test/java/tests/CalculatorTest.java
@@ -0,0 +1,74 @@
+package tests;
+
+import base.TestBase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import pages.CalculatorPage;
+
+public class CalculatorTest extends TestBase {
+    private CalculatorPage calculatorPage;
+
+    @Before
+    public void openPage (){
+        driver.get(BASE_URL+"/kalkulacka.php");
+        calculatorPage = new CalculatorPage(driver);
+    }
+
+    @Test
+    public void itShouldCount (){
+        calculatorPage.sendNumbers("7", "4");
+        driver.findElement(By.id("count")).click();
+        Assert.assertEquals("11",
+                calculatorPage.actualResult());
+
+    }
+
+    @Test
+    public void itShouldDeduce(){
+        calculatorPage.sendNumbers("7", "4");
+        driver.findElement(By.id("deduct")).click();
+        Assert.assertEquals("3",
+                calculatorPage.actualResult());
+    }
+
+    @Test
+    public void itShouldMultipli(){
+        calculatorPage.sendNumbers("7", "3");
+        driver.findElement(By.id("multiply")).click();
+        Assert.assertEquals("21", calculatorPage.actualResult());
+    }
+
+    @Test
+    public void itShouldDivide(){
+        calculatorPage.sendNumbers("7","3");
+        driver.findElement(By.id("divide")).click();
+        System.out.println(calculatorPage.actualResult());
+        Assert.assertTrue(calculatorPage.actualResult().contains("2.3"));
+    }
+
+    @Test
+    public void itShouldResetColums(){
+        calculatorPage.sendNumbers("7", "4");
+        driver.findElement(By.id("deduct")).click();
+        driver.findElement(By.id("reset")).click();
+        Assert.assertTrue(calculatorPage.getFirstInput().getAttribute("value").isEmpty());
+        Assert.assertTrue(calculatorPage.getSecondInput().getAttribute("value").isEmpty());
+    }
+
+    @Test
+    public void itShouldDisplayedLastCalculation(){
+        calculatorPage.sendNumbers("7", "2");
+        driver.findElement(By.id("count")).click();
+        Assert.assertEquals("7+2 = 9", calculatorPage.getLatestCalculation().getText());
+        Assert.assertEquals(1, calculatorPage.getListOfCalculation().size());
+    }
+
+
+
+
+}
